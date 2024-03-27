37e5a06739e7ca0977139b3e00faa941b782aeac
soniigx <soniagquin@gmail.com>
2024-03-27 15:12:53 -0400

    fix the button

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function downloadImages() {
-  var images = ["image1.jpg", "image2.jpg", "image3.jpg"]; // Add more images here
+function downloadAllJPGs() {
+  // Select all images with JPG format
+  var images = document.querySelectorAll('img[src$=".jpg"]');
 
-  images.forEach(function (image) {
+  images.forEach(function (image, index) {
     var link = document.createElement("a");
-    link.href = image;
-    link.download = "";
+    link.href = image.src;
+    link.download = "image_" + (index + 1) + ".jpg"; // Or any desired name
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
